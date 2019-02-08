7adbab3c954d0a62d50873265ac14553dd5022d2
vatsaashwin <aashwinvats@GMAIL.com>
2019-02-07 22:52:53 -0800

    changes to contact us and about

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,9 @@ class Food < ApplicationRecord
     has_attached_file :image,
         url: "/system/:hash.:extension",
         hash_secret: "ENTER HASH KEY",
+
+        # hash_secret: "a6af45ff44b8b2227721db2920dee251cf5f597bc463201dd33d97924e797d3cdd05fe765289c2efabc34ad3e3a0174a3e490b4b0f1a6dd9397d1567cbf3d300",
+
         
         styles: {
                     thumb: ["300x300", :jpeg],
